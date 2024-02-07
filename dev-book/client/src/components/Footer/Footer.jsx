bd04df9a1d572c7d3657e8ef76bda234d068e10f
lyklecharova <lklecharova9@gmail.com>
2024-02-07 11:15:56 +0200

    Create footer
    create css about footer

---
+++ dev-book/client/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import style from './Footer.module.css'
+export const Footer = () => {
+  return (
+    <footer className={style['footer']}>
+      <p>All rights reserved  January 2024 - &copy;</p>
+    </footer>
+  );
+};
