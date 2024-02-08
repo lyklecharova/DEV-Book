e73afa963ee86eba77bfe0020363bb05f46c680a
lyklecharova <lklecharova9@gmail.com>
2024-02-08 22:48:19 +0200

    Update userService.js

--- dev-book/client/service/userService.js
+++ dev-book/client/service/userService.js
@@ -1,29 +1,43 @@
 export async function register(user) {
     const url = `http://localhost:3030/user/register`;
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user)
-    });
-    const data = await response.json();
-    console.log(data)
-    return data;
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        });
+        if (!response.ok) {
+            throw new Error('Invalid registration');
+        }
+        const data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        throw new Error('Invalid registration');
+    }
 };
 
 export async function login(user) {
     const url = `http://localhost:3030/user/login`;
-
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user)
-    });
-    const data = await response.json();
-    console.log(data)
-    return data;
+    
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        });
+        if (!response.ok) {
+            throw new Error('Invalid login');
+        }
+        const data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        throw new Error('Invalid login');
+    }
 };
