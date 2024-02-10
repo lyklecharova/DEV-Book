57fa636bf2998786a78fd2ec85a861aabacac298
lyklecharova <lklecharova9@gmail.com>
2024-02-10 21:39:52 +0200

    Update Footer.jsx

--- dev-book/client/src/components/Footer/Footer.jsx
+++ dev-book/client/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import style from './Footer.module.css'
 export const Footer = () => {
   return (
     <footer className={style['footer']}>
-      <p>All rights reserved  January 2024 - &copy;</p>
+      <p>All rights reserved  January 2024 - February 2024 &copy;</p>
     </footer>
   );
 };
