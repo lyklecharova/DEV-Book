83b00a282138254c56064153384449a165c48fb2
lyklecharova <lklecharova9@gmail.com>
2024-02-11 10:12:06 +0200

    Create constants.js

---
+++ dev-book/server/constants/constants.js
@@ -0,0 +1,9 @@
+const JWT_SECRET = '0c387a88-7814-4e1e-bbdc-c87424a6d823';
+const PORT = 3030;
+const MONGO_DB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/dev-book'
+
+module.exports = {
+    JWT_SECRET,
+    PORT,
+    MONGO_DB_CONNECTION_STRING
+}
