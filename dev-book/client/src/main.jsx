ff8354bc5ef6ba4dfa6aa2e62a6b552193f19d3a
lyklecharova <lklecharova9@gmail.com>
2024-02-20 22:08:07 +0200

    Update main.jsx

--- dev-book/client/src/main.jsx
+++ dev-book/client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
+
+
