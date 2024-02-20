7b5ef5c0a4e519d552702f64b7c512d7cdd5997e
lyklecharova <lklecharova9@gmail.com>
2024-02-21 00:36:12 +0200

    Update Header.jsx
    Update header with dark mode theme

--- dev-book/client/src/components/Header/Header.jsx
+++ dev-book/client/src/components/Header/Header.jsx
@@ -1,81 +1,75 @@
-import { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDev } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDev } from "@fortawesome/free-brands-svg-icons";
 
-import style from './Header.module.css';
-import { AuthContext } from '../../contexts/authContext';
+import style from "./Header.module.css";
+import { AuthContext } from "../../contexts/authContext";
+import { ThemeToggle } from "../ThemeButton/ThemeButton";
+import { ThemeContext } from "../../contexts/ThemeContext";
+
 export const Header = () => {
     const { isLog } = useContext(AuthContext);
+    const { theme } = useContext(ThemeContext);
+
+    const colorLink = (name, link) => {
+        return (
+            <NavLink
+                style={({ isActive }) => ({
+                    color: isActive ? "#0077cc" : theme === "Light" ? "#2db583" : "#000",
+                })}
+                to={link}
+            >
+                {name}
+            </NavLink>
+        );
+    };
+
     return (
-
-        <header className={style['header']}>
-            <nav className={style['nav-bar']}>
-                <Link to="/">Dev-Book
-                    <div className={style['flav-icon']}>
-
+        <header className={style["header"]}>
+            <nav className={style["nav-bar"]}>
+                <Link to="/">
+                    Dev-Book
+                    <div className={style["flav-icon"]}>
                         <FontAwesomeIcon icon={faDev} />
                     </div>
                 </Link>
 
-
-                <ul role="list" className={style['ul-list']}>
-                    <li className={style['li-element']}>
-                        <NavLink style={({ isActive }) => ({
-                            color: isActive ? "#0077cc" : "#3F3F3F"
-                        })} to="/">
-                            Home
-                        </NavLink>
-
+                <ul role="list" className={style["ul-list"]}>
+                    <li className={style["li-element"]}>
+                        {colorLink("Home", "/")}
                     </li>
 
-                    <li className={style['li-element']}>
-                        <NavLink style={({ isActive }) => ({
-                            color: isActive ? "#0077cc" : "#3F3F3F"
-                        })} to="/catalog">
-                            Catalog
-                        </NavLink>
+                    <li className={style["li-element"]}>
+                        {colorLink("Catalog", "/catalog")}
                     </li>
 
                     {isLog && (
                         <>
-                            <li className={style['li-element']}>
-                                <NavLink style={({ isActive }) => ({
-                                    color: isActive ? "#0077cc" : "#3F3F3F"
-                                })} to="/create-books">
-                                    Add Book
-                                </NavLink>
+                            <li className={style["li-element"]}>
+                                {colorLink("Add Book", "/create-books")}
                             </li>
 
-                            <li className={style['li-element']}>
-                                <NavLink style={({ isActive }) => ({
-                                    color: isActive ? "#0077cc" : "#3F3F3F"
-                                })} to="/logout">
-                                    Logout
-                                </NavLink>
+                            <li className={style["li-element"]}>
+                                {colorLink("Logout", "/logout")}
                             </li>
                         </>
                     )}
                     {!isLog && (
                         <>
-                            <li className={style['li-element']}>
-                                <NavLink style={({ isActive }) => ({
-                                    color: isActive ? "#0077cc" : "#3F3F3F"
-                                })} to="/login">
-                                    Login
-                                </NavLink>
+                            <li className={style["li-element"]}>
+                                {colorLink("Login", "/login")}
                             </li>
 
-                            <li className={style['li-element']}>
-                                <NavLink style={({ isActive }) => ({
-                                    color: isActive ? "#0077cc" : "#3F3F3F"
-                                })} to="/register">
-                                    Register
-                                </NavLink>
+                            <li className={style["li-element"]}>
+                                {colorLink("Register", "/register")}
                             </li>
                         </>
                     )}
+                    <li>
+                        <ThemeToggle />
+                    </li>
                 </ul>
             </nav>
         </header>
